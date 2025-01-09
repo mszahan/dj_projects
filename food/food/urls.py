403a83ca70afd29dfd2d56a4e1e325f53c966d6a
mszahan <mszahan1@gmail.com>
2025-01-09 09:42:11 +0600

    implemented create_product view and forms

--- food/food/urls.py
+++ food/food/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='food_list' ),
-    path('<int:id>/', views.product_detail, name='food_detail')
+    path('<int:id>/', views.product_detail, name='food_detail'),
+    path('add-product', views.create_product, name='create_product'),
 ]
--- food/food/views.py
+++ food/food/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
+from .forms import ProductForm
 
 
 def index (request):
@@ -10,3 +11,23 @@ def index (request):
 def product_detail(request, id):
     product = get_object_or_404(Product, id=id)
     return render(request, 'food_detail.html', {'product':product})
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('food_list')
+    else:
+        form = ProductForm()
+    return render(request, 'product_form.html', {'form':form})
+
+
+# def create_product(request):
+#     form = ProductForm(request.POST or None)
+
+#     if form.is_valid():
+#         form.save()
+#         return redirect('food_list')
+#     return render(request, 'product_form.html', {'form':form})
+    
