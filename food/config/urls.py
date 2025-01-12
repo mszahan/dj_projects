7b0decc30ece82618419099de63f7c63fbb32294
mszahan <mszahan1@gmail.com>
2025-01-12 09:24:36 +0600

    added user registration endpoint with email and username

--- food/config/urls.py
+++ food/config/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('food.urls'))
+    path('', include('food.urls')),
+    path('users/', include('users.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ food/users/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
+from django.contrib import messages
+
+# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {username} your account is created')
+            return redirect('food_list')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form':form})
+
