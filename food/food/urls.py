9ed6c0d0bab0b04608cbe44643b2bc308f609370
mszahan <mszahan1@gmail.com>
2025-01-13 11:17:37 +0600

    automatically associated the user with food in class based create view

--- food/food/urls.py
+++ food/food/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.FoodListView.as_view(), name='food_list' ),
     # path('<int:id>/', views.product_detail, name='food_detail'),
     path('<int:pk>/', views.FoodDetailView.as_view(), name='food_detail'),
-    path('add-product/', views.create_product, name='create_product'),
+    # path('add-product/', views.create_product, name='create_product'),
+    path('add-product/', views.CreateProductView.as_view(), name='create_product'),
     path('update-product/<int:id>/', views.update_product, name='update_product'),
     path('delete/<int:id>/', views.delete_product, name='delete_product'),
 ]
--- food/food/views.py
+++ food/food/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Product
 from .forms import ProductForm
 
@@ -26,16 +27,25 @@ class FoodDetailView(DetailView):
     context_object_name = 'product' # default name will be object
 
 
-def create_product(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            # form.save()
-            product = form.save()
-            return redirect(product.get_absolute_url())
-    else:
-        form = ProductForm()
-    return render(request, 'product_form.html', {'form':form})
+# def create_product(request):
+#     if request.method == 'POST':
+#         form = ProductForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # form.save()
+#             product = form.save()
+#             return redirect(product.get_absolute_url())
+#     else:
+#         form = ProductForm()
+#     return render(request, 'product_form.html', {'form':form})
+
+class CreateProductView(CreateView):
+    model = Product
+    fields = ['name', 'description', 'price', 'image']
+    template_name = 'product_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
 def update_product(request, id):
