85283d997cc3a0c47c6d081a252eea0d58b2439e
mszahan <mszahan1@gmail.com>
2025-01-16 12:56:26 +0600

    terminated the project for malpractice

--- site_scrap/scraper/models.py
+++ site_scrap/scraper/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Link(models.Model):
+    address = models.TextField(max_length=1000,blank=True, null=True)
+    name = models.TextField(max_length=1000,blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
--- site_scrap/scraper/views.py
+++ site_scrap/scraper/views.py
@@ -1,3 +1,18 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+from .models import Link
 
-# Create your views here.
+
+def scrape(request):
+    page = requests.get('https://facebook.com')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    for link in soup.find_all('a'):
+        link_address = link.get('href')
+        link_text = link.string
+        Link.objects.create(address=link_address, name=link_text)
+    links = Link.objects.all()
+    return render(request, 'result.html', {'links':links})
+
+
+
