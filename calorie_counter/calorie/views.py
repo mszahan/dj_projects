c90c11f87cbaa44b16a4679ce6c4530b32833ee7
mszahan <mszahan1@gmail.com>
2025-01-19 11:29:47 +0600

    added the consumed food in table with bootstrap

--- calorie_counter/calorie/views.py
+++ calorie_counter/calorie/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from .models import Food
+from django.shortcuts import render, redirect
+from .models import Food, Consume
 
 
 def index(request):
+    if request.method == 'POST':
+        food_id = request.POST['food_consumed']
+        food = Food.objects.get(id=food_id)
+        user = request.user
+        consume = Consume(user=user, food=food)
+        consume.save()
+        return redirect('food_list')
     foods = Food.objects.all()
-    return render(request, 'index.html', {'foods':foods})
+    consume_foods = Consume.objects.filter(user=request.user)
+    return render(request, 'index.html', {'foods':foods,'consume_foods':consume_foods })
+
+
