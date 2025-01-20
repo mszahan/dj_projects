90249b778f163eba689567427a990231f3f7c70e
mszahan <mszahan1@gmail.com>
2025-01-20 11:51:13 +0600

    added delete option for consumed food

--- calorie_counter/calorie/urls.py
+++ calorie_counter/calorie/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='food_list'),
+    path('delete/<int:id>', views.delete_consume, name='delete_consume'),
 ]
--- calorie_counter/calorie/views.py
+++ calorie_counter/calorie/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect,get_object_or_404
+# from django.contrib.auth.decorators import login_required
 from .models import Food, Consume
 
-@login_required
+# @login_required
 def index(request):
     if request.method == 'POST':
         food_id = request.POST['food_consumed']
@@ -16,3 +16,9 @@ def index(request):
     return render(request, 'index.html', {'foods':foods,'consume_foods':consume_foods })
 
 
+def delete_consume(request, id):
+    consumed_food = get_object_or_404(Consume, id=id, user=request.user)
+    if request.method == 'POST':
+        consumed_food.delete()
+        return redirect('food_list')
+    return render(request, 'delete.html')
