5fdff2d1a096bc8146ee24a592a53c9e526a14fb
mszahan <mszahan1@gmail.com>
2025-01-20 10:58:37 +0600

    added doughnut chart with chartjs

--- calorie_counter/calorie/views.py
+++ calorie_counter/calorie/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Food, Consume
 
-
+@login_required
 def index(request):
     if request.method == 'POST':
         food_id = request.POST['food_consumed']
