7983950cc0932d51751b01119cd31c94d9291b84
mszahan <mszahan1@gmail.com>
2025-01-21 10:50:05 +0600

    added login functionality to social app

--- social/users/views.py
+++ social/users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate,login
+from .forms import LoginForm
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponse('Login successfull')
+            else:
+                return HttpResponse('Invalid credentials')
+            
+    form = LoginForm()
+    return render(request, 'login.html', {'form':form} )
