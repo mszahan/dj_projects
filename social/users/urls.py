50831e49b04491552cbc236c7e2289809bf221db
mszahan <mszahan1@gmail.com>
2025-01-26 11:16:28 +0600

    added user registration functionality

--- social/users/urls.py
+++ social/users/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('register/', views.register, name='register'),
+    path('register-complete/', views.register_compelete, name='register_complete'),
     path('login/', views.user_login, name='login'),
     path('logout', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     
--- social/users/views.py
+++ social/users/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request):
@@ -24,3 +24,20 @@ def user_login(request):
 @login_required
 def homepage(request):
     return render(request, 'index.html', {})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST) 
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return redirect('register_complete')
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'register.html', {'user_form': user_form})
+
+
+def register_compelete(request):
+    return render(request, 'register_complete.html')
