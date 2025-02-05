818ccd9b878456e22d516c1cf6bb3946a010412d
mszahan <mszahan1@gmail.com>
2025-02-05 22:59:27 +0600

    displayed weekly, monthly and yearly expense sum

--- expense_tracker/expense/views.py
+++ expense_tracker/expense/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.shortcuts import render,redirect, get_object_or_404
 from django.db.models import Sum
 from .forms import ExpenseForm
@@ -12,10 +13,33 @@ def index(request):
             return redirect('homepage')
     expenses = Expense.objects.all()
     total_expenses = expenses.aggregate(Sum('amount'))
+    
+    #logic to calculate 365 days expenses
+    last_year = datetime.date.today() - datetime.timedelta(days=365)
+    #data is greater than last year
+    last_year_expense = Expense.objects.filter(date__gt=last_year)
+    yearly_sum = last_year_expense.aggregate(Sum('amount'))
+    
+    #logic to calculate 30 days expenses
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    #data is greater than last year
+    last_month_expense = Expense.objects.filter(date__gt=last_month)
+    monthly_sum = last_month_expense.aggregate(Sum('amount'))
+    
+    #logic to calculate 7 days expenses
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    #data is greater than last year
+    last_week_expense = Expense.objects.filter(date__gt=last_week)
+    weekly_sum = last_week_expense.aggregate(Sum('amount'))
+
     expense_form = ExpenseForm()
     return render(request,'index.html', {'expense_form':expense_form, 
                                          'expenses':expenses, 
-                                         'total_expenses':total_expenses})
+                                         'total_expenses':total_expenses,
+                                         'yearly_sum':yearly_sum,
+                                         'monthly_sum':monthly_sum,
+                                         'weekly_sum':weekly_sum,
+                                         })
 
 
 def edit(request, id):
