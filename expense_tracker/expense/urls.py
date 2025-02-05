ab3b93f71a60404debdca4ee8dc43d9bf25c26a5
mszahan <mszahan1@gmail.com>
2025-02-05 22:08:56 +0600

    added edit and delete funtionality

--- expense_tracker/expense/urls.py
+++ expense_tracker/expense/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='homepage' )
+    path('',views.index, name='homepage' ),
+    path('edit/<int:id>/', views.edit, name='edit_expense'),
+    path('delete/<int:id>/', views.delete, name='delete_expense'),
 ]
--- expense_tracker/expense/views.py
+++ expense_tracker/expense/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from .forms import ExpenseForm
 from .models import Expense
 
@@ -12,3 +12,21 @@ def index(request):
     expenses = Expense.objects.all()
     expense_form = ExpenseForm()
     return render(request,'index.html', {'expense_form':expense_form, 'expenses':expenses})
+
+
+def edit(request, id):
+    expense = get_object_or_404(Expense, id=id)
+    expense_form = ExpenseForm(instance=expense)
+    if request.method == 'POST':
+        expense_form = ExpenseForm(request.POST, instance=expense)
+        if expense_form.is_valid():
+            expense_form.save()
+            return redirect('homepage')
+    return render(request, 'edit.html', {'expense_form':expense_form})
+
+def delete(request, id):
+    # just to make sure the delete is in the post method since two post method in same page
+    if request.method == 'POST' and 'delete' in request.POST:
+        expense = get_object_or_404(Expense, id=id)
+        expense.delete()
+        return redirect('homepage')
