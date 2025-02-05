3b7b5be02d750d88ba415f1d9626c07108dedc25
mszahan <mszahan1@gmail.com>
2025-02-05 18:20:58 +0600

    added expense addition form

--- expense_tracker/expense/views.py
+++ expense_tracker/expense/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import ExpenseForm
+from .models import Expense
 
-# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        expense_form = ExpenseForm(request.POST)
+        if expense_form.is_valid():
+            expense_form.save()
+            return redirect('homepage')
+    expenses = Expense.objects.all()
+    expense_form = ExpenseForm()
+    return render(request,'index.html', {'expense_form':expense_form, 'expenses':expenses})
