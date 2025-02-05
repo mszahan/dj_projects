48ecf71d5799f918858c9e71d1b0fba4859fe556
mszahan <mszahan1@gmail.com>
2025-02-05 21:27:06 +0600

    added the expense table

--- expense_tracker/config/settings.py
+++ expense_tracker/config/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Ensure this path is correct
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
