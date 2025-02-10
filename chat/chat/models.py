9125ac2e4e3017a229496d9dfdbead7a3166b721
mszahan <mszahan1@gmail.com>
2025-02-10 16:16:54 +0600

    created chatroom detail page

--- chat/chat/models.py
+++ chat/chat/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class ChatRoom(models.Model):
@@ -8,4 +9,7 @@ class ChatRoom(models.Model):
     def __str__(self):
         return self.name
     
+
+    def get_absolute_url(self):
+        return reverse('chatroom', args=[self.slug])
 
--- chat/chat/views.py
+++ chat/chat/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ChatRoom
 
 
 def index(request):
     chatrooms = ChatRoom.objects.all()
     return render(request, 'index.html', {'chatrooms': chatrooms})
+
+def chatroom(request, slug):
+    chatroom = get_object_or_404(ChatRoom, slug=slug)
+    return render(request, 'room.html', {'chatroom':chatroom})
