18ef9f7a46b05db70f1f2adec69e0ed4b4f40116
mszahan <mszahan1@gmail.com>
2025-10-02 18:56:02 +0600

    serialized user and added permission to snippets endpoints

--- drf_docs/snippets/views.py
+++ drf_docs/snippets/views.py
@@ -3,20 +3,38 @@
 # from rest_framework import status
 # from rest_framework.response import Response
 # from rest_framework import mixins
+from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework import permissions
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 
+
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 # mixins -----------------------------------------
